d3e45930e662788ef50185830f7a3e93ba6fafa4
Peter Larsen <petertretorn@gmail.com>
2018-10-16 00:26:46 +0200

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,14 +4,7 @@
 
 export const environment = {
   production: false,
-  firebase: {
-    apiKey: "AIzaSyBte3eUDrGLk0T9jLzyVlrr3fNnUF7vrww",
-    authDomain: "leadreader-5b24c.firebaseapp.com",
-    databaseURL: "https://leadreader-5b24c.firebaseio.com",
-    projectId: "leadreader-5b24c",
-    storageBucket: "leadreader-5b24c.appspot.com",
-    messagingSenderId: "215384017835"
-  }
+  
 };
 
 /*
